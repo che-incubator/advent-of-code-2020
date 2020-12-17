7ae8504fbf3ccf7b5a57ef6ff4da611126e37647
jpinkney-1 <jpinkney@redhat.com>
2020-12-17 18:31:59 +0000

    Day 15 solutions
    Signed-off-by: jpinkney-1 <jpinkney@redhat.com>

---
+++ day-15/day-15.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	input := []int{8, 11, 0, 19, 1, 2 }
+
+	// Part 1
+	game(input,  2020)
+
+	// Part 2
+	game(input, 30000000)
+}
+
+func game(initialArr []int, totalTurns int) {
+	speakMap := make(map[int]int)
+	initial := initialArr[0]
+	var previousNum int
+	totalNumsSpoken := 0
+	for totalNumsSpoken < totalTurns {
+		previousNum = initial
+
+		if totalNumsSpoken < len(initialArr) {
+			initial = initialArr[totalNumsSpoken]
+		} else if _, ok := speakMap[initial]; ok {
+			initial = totalNumsSpoken - speakMap[initial] - 1
+		} else {
+			initial = 0
+		}
+
+		speakMap[previousNum] = totalNumsSpoken - 1
+		totalNumsSpoken += 1
+	}
+
+	fmt.Printf("For %d turns the last number spoken is: %d\n", totalTurns, initial)
+}
